a9e5652e8b303b17b0e786eddf67d6ad3c764be5
havoriskova <havoriskova@gmail.com>
2020-11-03 14:26:31 +0100

    Added CSS
    - added background, styling and class ".playing" which is on only while playing the sound

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ function playSound(e) {
 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         console.log(audio); // this shows the selected audio-element
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
     if (!audio) return; // = if there´s no audio (null), the function stops running
-
-    audio.play();
-
+    
+    audio.currentTime=0; // without this line the event has no effect when you press the button multipletimes
+    audio.play(); 
+    key.classList.add(`playing`);
 }
 
 
+function removeTransition(e) { // for CSS changing
+    if (e.propertyName !== `transform`) return;
+    e.target.classList.remove(`playing`); 
+}
 
+
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener(`transitionend`, removeTransition));
 window.addEventListener(`keydown`, playSound);
